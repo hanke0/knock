1ddc06e975894aa19adc0fb2b1f6120a15c8e657
hanke0 <hanke0@outlook.com>
2024-02-19 14:26:14 +0800

    support flush

--- knock.go
+++ knock.go
@@ -27,14 +27,18 @@ func getCSRF() string {
 	return string(b)
 }
 
-func writeHeader(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+func writeHeader(w http.ResponseWriter, raw bool) {
+	if raw {
+		w.Header().Set("Content-Type", "text/plain")
+	} else {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	}
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.Header().Set("X-Frame-Options", "DENY")
 	w.Header().Set("X-XSS-Protection", "1; mode=block")
 	w.Header().Set("Referrer-Policy", "no-referrer")
-	w.Header().Add("Set-Cookie", fmt.Sprintf("csrf-token=%s; Max-Age=3000; Path=/knock; Secure; HttpOnly; SameSite=Strict", getCSRF()))
+	w.Header().Add("Set-Cookie", fmt.Sprintf("csrf-token=%s; Max-Age=3000; Path=/knock; Secure; SameSite=Strict", getCSRF()))
 }
 
 func normalizeIP(ip string) string {
@@ -52,27 +56,9 @@ func normalizeIP(ip string) string {
 	return a.String()
 }
 
-var nftlist = `
-table inet knock-table {
-	set addr-set-sshd {
-			type ipv4_addr
-			elements = { }
-	}
+var nftadd = `add element inet filter addr-set-sshd { %s }`
 
-	chain knock-table {
-			type filter hook input priority filter - 1; policy accept;
-			tcp dport { %d } ip saddr @addr-set-sshd reject with icmp port-unreachable
-	}
-}
-`
-
-var nftadd = `
-add element inet knock-table addr-set-sshd { %s }
-`
-
-var nftdel = `
-delete element inet knock-table addr-set-sshd { %s }
-`
+var nftdel = `flush set inet filter addr-set-sshd`
 
 type Buffer struct {
 	b bytes.Buffer
@@ -88,7 +74,7 @@ func (b *Buffer) Write(p []byte) (n int, err error) {
 func nftExecf(format string, args ...interface{}) (string, bool) {
 	var buf Buffer
 	var stdin bytes.Buffer
-	fmt.Fprintf(&buf, format, args...)
+	fmt.Fprintf(&stdin, format, args...)
 	cmd := exec.Command("nft", "-f", "-")
 	cmd.Stdin = &stdin
 	cmd.Stdout = &buf
@@ -106,10 +92,33 @@ func allowIP(ip string) bool {
 	return ok
 }
 
-func disallowIP(ip string) bool {
-	s, ok := nftExecf(nftdel, ip)
-	log.Printf("nft del: %s, success=%v, result: %s", ip, ok, s)
-	return ok
+func checkCSRF(w http.ResponseWriter, r *http.Request) bool {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return false
+	}
+
+	token := r.FormValue("csrf-token")
+	if token == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return false
+	}
+	var csrf string
+	for _, v := range r.Cookies() {
+		if v.Name == "csrf-token" {
+			csrf = v.Value
+			break
+		}
+	}
+	if csrf == "" {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return false
+	}
+	if token != csrf {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return false
+	}
+	return true
 }
 
 func main() {
@@ -124,15 +133,35 @@ func main() {
 	if port < 1 || port > 65535 {
 		panic("bad port")
 	}
-	s, ok := nftExecf(nftlist, port)
-	log.Printf("nft list: success=%v, result: %s", ok, s)
-	if !ok {
-		panic("nft list failed")
+	if v := os.Getenv("NFTABLES_ENABLE_ACTION"); v != "" {
+		nftadd = v
+	}
+	if v := os.Getenv("NFTABLES_FLUSH_ACTION"); v != "" {
+		nftdel = v
+	}
+
+	log.Printf("nft add action: %s", nftadd)
+	log.Printf("nft flush action: %s", nftdel)
+	if nftdel != "" {
+		http.HandleFunc("/knock/flush", func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != "POST" {
+				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+				return
+			}
+			if !checkCSRF(w, r) {
+				return
+			}
+			s, ok := nftExecf(nftdel)
+			log.Printf("nft flush: success=%v, result: %s", ok, s)
+			writeHeader(w, true)
+			w.WriteHeader(200)
+			fmt.Fprintf(w, "OK: %v", ok)
+		})
 	}
 
 	http.HandleFunc("/knock", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
-			writeHeader(w)
+			writeHeader(w, false)
 			w.Header().Set("Content-Length", strconv.Itoa(len(indexHTML)))
 			w.WriteHeader(200)
 			w.Write(indexHTML)
@@ -147,35 +176,14 @@ func main() {
 			http.Error(w, "Bad IP", http.StatusBadRequest)
 			return
 		}
-
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
+		if !checkCSRF(w, r) {
 			return
 		}
 
-		token := r.FormValue("csrf-token")
-		if token == "" {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
-			return
-		}
-		var csrf string
-		for _, v := range r.Cookies() {
-			if v.Name == "csrf-token" {
-				csrf = v.Value
-				break
-			}
-		}
-		if csrf == "" {
-			http.Error(w, "Forbidden", http.StatusForbidden)
-			return
-		}
-		if token != csrf {
-			http.Error(w, "Forbidden", http.StatusForbidden)
-			return
-		}
 		ok := allowIP(ip)
-		writeHeader(w)
+		writeHeader(w, true)
 		w.WriteHeader(200)
+		fmt.Fprintln(w, ip)
 		fmt.Fprintf(w, "OK: %v", ok)
 	})
 
