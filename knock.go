e278151bd750c7dc22f0677eae2a23a01f8b22fe
hanke <hanke0@outlook.com>
2024-06-27 11:34:05 +0000

    support ipv6

--- knock.go
+++ knock.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	_ "embed"
-	"log"
-	"sync"
 
 	"bytes"
 	"fmt"
+	"log"
 	"math/rand"
 	"net"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
+	"sync"
 )
 
 //go:embed index.html
@@ -41,24 +41,23 @@ func writeHeader(w http.ResponseWriter, raw bool) {
 	w.Header().Add("Set-Cookie", fmt.Sprintf("csrf-token=%s; Max-Age=3000; Path=/knock; Secure; SameSite=Strict", getCSRF()))
 }
 
-func normalizeIP(ip string) string {
+func normalizeIP(ip string) net.IP {
 	a := net.ParseIP(ip)
 	if a == nil {
-		return ""
+		return nil
 	}
 	if !a.IsGlobalUnicast() {
-		return ""
+		return nil
 	}
-	a = a.To4()
-	if a == nil {
-		return ""
-	}
-	return a.String()
+	return a
 }
 
-var nftadd = `add element inet filter addr-set-sshd { %s }`
+var nftAddIPv4 = `add element inet filter addr-set-sshd { %s }`
+var nftAddIPv6 = `add element inet filter ipv6-addr-set-sshd { %s }`
 
-var nftdel = `flush set inet filter addr-set-sshd`
+var nftdel = `
+flush set inet filter addr-set-sshd; 
+flush set inet filter ipv6-addr-set-sshd;`
 
 type Buffer struct {
 	b bytes.Buffer
@@ -86,8 +85,16 @@ func nftExecf(format string, args ...interface{}) (string, bool) {
 	return buf.b.String(), true
 }
 
-func allowIP(ip string) bool {
-	s, ok := nftExecf(nftadd, ip)
+func allowIP(ip net.IP) bool {
+	var (
+		s  string
+		ok bool
+	)
+	if v := ip.To4(); len(v) == net.IPv4len {
+		s, ok = nftExecf(nftAddIPv4, v.String())
+	} else {
+		s, ok = nftExecf(nftAddIPv6, ip.String())
+	}
 	log.Printf("nft delete: %s, success=%v, result: %s", ip, ok, s)
 	return ok
 }
@@ -140,14 +147,18 @@ func main() {
 	if port < 1 || port > 65535 {
 		panic("bad port")
 	}
-	if v := os.Getenv("NFTABLES_ENABLE_ACTION"); v != "" {
-		nftadd = v
+	if v := os.Getenv("NFTABLES_ENABLE_IPV4_ACTION"); v != "" {
+		nftAddIPv4 = v
+	}
+	if v := os.Getenv("NFTABLES_ENABLE_IPV6_ACTION"); v != "" {
+		nftAddIPv6 = v
 	}
 	if v := os.Getenv("NFTABLES_FLUSH_ACTION"); v != "" {
 		nftdel = v
 	}
 
-	log.Printf("nft add action: %s", nftadd)
+	log.Printf("nft add ipv4 action: %s", nftAddIPv4)
+	log.Printf("nft add ipv6 action: %s", nftAddIPv6)
 	log.Printf("nft flush action: %s", nftdel)
 	if nftdel != "" {
 		http.HandleFunc("/knock/flush", func(w http.ResponseWriter, r *http.Request) {
@@ -179,7 +190,7 @@ func main() {
 			return
 		}
 		ip := normalizeIP(r.Header.Get("X-Real-IP"))
-		if ip == "" {
+		if ip == nil {
 			http.Error(w, "Bad IP", http.StatusBadRequest)
 			return
 		}
